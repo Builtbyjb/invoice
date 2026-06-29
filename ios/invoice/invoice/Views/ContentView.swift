b90565c658d3b54306acbbb74a657b1e9c6227f1
Builtbyjb <awotideajibola@gmail.com>
2026-06-29 13:43:04 -0400

    tabView now use non-fill icon variant

--- ios/invoice/invoice/Views/ContentView.swift
+++ ios/invoice/invoice/Views/ContentView.swift
@@ -8,20 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab = 0
+
     var body: some View {
-        TabView {
-            HomeView().tabItem {
-                Label("Home", systemImage: "house")
-            }
-            ClientsView().tabItem {
-                Label("Clients", systemImage: "person.fill")
-            }
-            InvoicesView().tabItem {
-                Label("Invoices", systemImage: "doc.text.fill")
-            }
-            ReferralView().tabItem {
-                Label("Referral", systemImage: "gift.fill")
-            }
+        TabView(selection: $selectedTab) {
+            HomeView()
+                .tag(0)
+                .tabItem {
+                    Label {
+                        Text("Home")
+                    } icon: {
+                        Image(
+                            uiImage: UIImage(
+                                systemName: selectedTab == 0 ? "house.fill" : "house"
+                            )!
+                        )
+                        .renderingMode(.template)
+                    }
+                }
+            ClientsView()
+                .tag(1)
+                .tabItem {
+                    Label {
+                        Text("Clients")
+                    } icon: {
+                        Image(
+                            uiImage: UIImage(
+                                systemName: selectedTab == 1 ? "person.fill" : "person"
+                            )!
+                        )
+                        .renderingMode(.template)
+                    }
+                }
+            InvoicesView()
+                .tag(2)
+                .tabItem {
+                    Label {
+                        Text("Invoices")
+                    } icon: {
+                        Image(
+                            uiImage: UIImage(
+                                systemName: selectedTab == 2 ? "doc.text.fill" : "doc.text"
+                            )!
+                        )
+                        .renderingMode(.template)
+                    }
+                }
+            ReferralView()
+                .tag(3)
+                .tabItem {
+                    Label {
+                        Text("Referral")
+                    } icon: {
+                        Image(
+                            uiImage: UIImage(
+                                systemName: selectedTab == 3 ? "gift.fill" : "gift"
+                            )!
+                        )
+                        .renderingMode(.template)
+                    }
+                }
         }
     }
 }
